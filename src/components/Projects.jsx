6c4951bede5fe3f331a1fa6ece2a80dcee8ef90f
Aakash Saini <121600651+aakashsaini09@users.noreply.github.com>
2024-07-22 20:46:55 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -33,7 +33,7 @@ const Projects = () => {
             <img src={coin} alt="" />
             <div className="name">Crypto-Monster</div>
             <div className="paragraph">
-              Crypto tracking application that offers price updates & one-year data trends. Developed with React/TypeScript, Chart API, TailWind-CSS and more.
+              Crypto tracking application that offers price updates & one-year data trends. Developed with React/TypeScript, Chart API, TailWind-CSS and more.<br /><br /><span>Tech Stack: </span>TYPESCRIPT, REACT-JS, TAILWIND-CSS.
               </div>
             <div className="icons" >
               <div className="icon">
@@ -45,24 +45,9 @@ const Projects = () => {
             </div>
           </div>
             <div className="project-container" data-aos="zoom-in-up">
-              <img src={stock} alt="" />
-              <div className="name">Stock Management</div>
-              <div className="paragraph">
-                It's a Full-stack Web Application that seamlessly integrates React-JS, Node-JS, MongoDB, and Express-JS and manage user stock details. The design is elevated by the modern aesthetics of Tailwind CSS, ensuring sleek styling and enhanced user interaction.<br /><br /><span>Tech Stack:</span>JAVASCRIPT, REACT-JS, MONGO-DB, TAILWIND-CSS.
-              </div>
-              <div className="icons" >
-                <div className="icon">
-                  <a target='_blank' href="https://stock-management-aakashsaini09.vercel.app/"><i className="fa-solid fa-eye" style={{ color: "#ffffff" }}></i></a>
-                </div>
-                <div className="icon">
-                  <a target='_blank' href="https://github.com/aakashsaini09/stock"><i className="fa-brands fa-github" style={{ color: "#ffffff" }}></i></a>
-                </div>
-              </div>
-            </div>
-            <div className="project-container" data-aos="zoom-in-up">
               <img className='special-case' src={github} alt="" />
               <div className="name">Github Profile Search</div>
-              <div className="paragraph">Its a simple react application to search Github profile and view some basic details. It uses Github API and fetch profile data from github.<br /><br /><span>Tech Stack:</span>TYPESCRIPT, REACT-JS, TAILWIND-CSS.
+              <div className="paragraph">Its a simple react application to search Github profile and view some basic details. It uses Github API and fetch profile data from github.<br /><br /><span>Tech Stack: </span>TYPESCRIPT, REACT-JS, TAILWIND-CSS.
               </div>
               <div className="icons">
                 <div className="icon">
@@ -77,11 +62,26 @@ const Projects = () => {
                 </div>
               </div>
             </div>
+           <div className="project-container" data-aos="zoom-in-up">
+              <img src={stock} alt="" />
+              <div className="name">Stock Management</div>
+              <div className="paragraph">
+                It's a Full-stack Web Application that seamlessly integrates React-JS, Node-JS, MongoDB, and Express-JS and manage user stock details. The design is elevated by the modern aesthetics of Tailwind CSS, ensuring sleek styling and enhanced user interaction.<br /><br /><span>Tech Stack: </span>JAVASCRIPT, REACT-JS, MONGO-DB, TAILWIND-CSS.
+              </div>
+              <div className="icons" >
+                <div className="icon">
+                  <a target='_blank' href="https://stock-management-aakashsaini09.vercel.app/"><i className="fa-solid fa-eye" style={{ color: "#ffffff" }}></i></a>
+                </div>
+                <div className="icon">
+                  <a target='_blank' href="https://github.com/aakashsaini09/stock"><i className="fa-brands fa-github" style={{ color: "#ffffff" }}></i></a>
+                </div>
+              </div>
+            </div>
             <div className="project-container" data-aos="zoom-in-up">
               <img src={todo} alt="" />
               <div className="name">TODO-List</div>
               <div className="paragraph">
-                It's a web app to store your important notes in your browser. You can create notes in TODO-List webpage and it will remain there even if you reload your page or come back after days.<br /><br /><span>Tech Stack:</span> React-JS, SCSS, HTML, JAVASCRIPT
+                It's a web app to store your important notes in your browser. You can create notes in TODO-List webpage and it will remain there even if you reload your page or come back after days.<br /><br /><span>Tech Stack: </span> React-JS, SCSS, HTML, JAVASCRIPT
               </div>
               <div className="icons">
                 <div className="icon">
@@ -96,7 +96,7 @@ const Projects = () => {
               <img src={portfolio} alt="" />
               <div className="name">Portfolio</div>
               <div className="paragraph">
-                This is my new portfolio, which I recently made using React-JS. It is a private repository to protect the code. You can give me feedback about the website and other projects from the contact section that I have provided at the bottom of the website..<br /><br /><span>Tech Stack:</span>JAVASCRIPT, REACT-JS, TAILWIND-CSS.
+                This is my new portfolio, which I recently made using React-JS. It is a private repository to protect the code. You can give me feedback about the website and other projects from the contact section that I have provided at the bottom of the website..<br /><br /><span>Tech Stack: </span>JAVASCRIPT, REACT-JS, TAILWIND-CSS.
               </div>
               <div className="icons">
                 <div className="icon">
